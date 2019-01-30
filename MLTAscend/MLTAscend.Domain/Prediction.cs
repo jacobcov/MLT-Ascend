3c28e0d7116159258604d1cd8f6532ce1b4c263a
Mason Degeneffe <degeneffem@gmail.com>
2019-01-29 19:57:08 -0500

    Create Prediction Class

---
+++ MLTAscend/MLTAscend.Domain/Prediction.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MLTAscend.Domain
+{
+    public class Prediction
+    {
+        public string CompanyName { get; set; }
+        public DateTime CreationDate { get; set; }
+
+        public double OneWeekPred { get; set; }
+        public double OneMonthPred { get; set; }
+        public double ThreeMonthPred { get; set; }
+        public double SixMonthPred { get; set; }
+        public double OneYearPred { get; set; }
+        public double TwoYearPred { get; set; }
+        public double FiveYearPred { get; set; }
+    }
+}
