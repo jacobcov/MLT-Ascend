cb5938f0a10a850febfe3404947cc6154f565b0f
Mason Degeneffe <degeneffem@gmail.com>
2019-01-30 19:29:16 -0500

    Create GetUserByUsername and Set User

--- MLTAscend/MLTAscend.Data/Helpers/UserHelper.cs
+++ MLTAscend/MLTAscend.Data/Helpers/UserHelper.cs
@@ -1,10 +1,24 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using dom = MLTAscend.Domain.Models;
+using System.Linq;
 
 namespace MLTAscend.Data.Helpers
 {
     class UserHelper
     {
+        private static MLTAscendDbContext _db = new MLTAscendDbContext();
+
+        public dom.User GetUserByUsername(string username)
+        {
+            return _db.Users.FirstOrDefault(m => m.Username == username);
+        }
+
+        public bool SetUser(dom.User user)
+        {
+            _db.Users.Add(user);
+            return _db.SaveChanges() > 0;
+        }
     }
 }
