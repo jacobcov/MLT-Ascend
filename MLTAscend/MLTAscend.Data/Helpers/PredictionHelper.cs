4388c1ec967daee0f540c1f4fa12bb3f3738a18a
Mason Degeneffe <degeneffem@gmail.com>
2019-01-30 19:18:38 -0500

    Create PredictionHelper class

---
+++ MLTAscend/MLTAscend.Data/Helpers/PredictionHelper.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.EntityFrameworkCore;
+using dom = MLTAscend.Domain.Models;
+using System.Linq;
+
+
+namespace MLTAscend.Data.Helpers
+{
+    class PredictionHelper
+    {
+        private static MLTAscendDbContext _db = new MLTAscendDbContext();
+
+        public dom.User GetUser(string username)
+        {
+            return _db.Users.FirstOrDefault(m => m.Username == username);
+        }
+    }
+}
