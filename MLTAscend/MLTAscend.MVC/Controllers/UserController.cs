eae6d1b0791490bf68cb42c461ab554fe3bd7549
Nate Johnson <johnson.nate.c@gmail.com>
2019-01-31 00:23:28 -0500

    added a User Controller and the SignUp/LogIn razor files [finish #163612579]

---
+++ MLTAscend/MLTAscend.MVC/Controllers/UserController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MLTAscend.MVC.Controllers
+{
+  public class UserController : Controller
+  {
+    public IActionResult LogIn()
+    {
+      return View("_LogIn");
+    }
+
+    public IActionResult SignUp()
+    {
+      return View("_SignUp");
+    }
+  }
+}
