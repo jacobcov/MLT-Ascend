fc2a0ca84d3c32a2d5e01cf8d58a2e248305390a
Nate Johnson <johnson.nate.c@gmail.com>
2019-02-01 07:13:28 -0500

    added a UserViewModel [#163650010]

---
+++ MLTAscend/MLTAscend.MVC/ViewModels/UserViewModel.cs
@@ -0,0 +1,18 @@
+﻿using MLTAscend.Data.Helpers;
+using MLTAscend.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MLTAscend.MVC.ViewModels
+{
+  public class UserViewModel
+  {
+    public List<User> GetUsers()
+    {
+      var uh = new UserHelper();
+      return uh.GetUsers();
+    }
+  }
+}
