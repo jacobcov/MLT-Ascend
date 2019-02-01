ddb1dd46b223e667b49ba874a34d133a718a1d69
Nate Johnson <johnson.nate.c@gmail.com>
2019-02-01 07:21:41 -0500

    created an InUser [#163650010]

--- MLTAscend/MLTAscend.MVC/Controllers/UserController.cs
+++ MLTAscend/MLTAscend.MVC/Controllers/UserController.cs
@@ -3,14 +3,28 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using dom = MLTAscend.Domain.Models;
 
 namespace MLTAscend.MVC.Controllers
 {
   public class UserController : Controller
   {
-    public IActionResult LogIn()
+    //[Route("Home/LogIn")]
+    public IActionResult LogIn(User signIn)
     {
-      return View("_LogIn");
+      if (ModelState.IsValid)
+      {
+        var uvm = new UserViewModel();
+        var user = uvm.GetUsers().FirstOrDefault(u => u.Username == signIn.Username);
+
+        if (user != null && signIn.Password == user.Password)
+        {
+          HttpContext.Session.SetString("user", JsonConvert.SerializeObject(user));
+
+          return RedirectToAction("Start", "Order");
+        };
+      }
+      return RedirectToAction("SignUp", "Home");
     }
 
     public IActionResult SignUp()
---
+++ MLTAscend/MLTAscend.MVC/Models/InUser.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PizzaStore.MvcClient.Models
+{
+  public class InUser
+  {
+    [Required]
+    [MinLength(4)]
+    [MaxLength(50)]
+    public string Username { get; set; }
+
+    [Required]
+    [MinLength(4)]
+    [MaxLength(50)]
+    public string Password { get; set; }
+  }
+}
