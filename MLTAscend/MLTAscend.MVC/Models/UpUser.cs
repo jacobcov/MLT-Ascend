7a4d92cafe0766b3eb2ff2a88449db3bc0dee750
Nate Johnson <johnson.nate.c@gmail.com>
2019-02-05 09:19:44 -0500

    added a UpUser model [start #163650058]

---
+++ MLTAscend/MLTAscend.MVC/Models/UpUser.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MLTAscend.MVC.Models
+{
+  public class UpUser
+  {
+    [Required]
+    [MinLength(4)]
+    [MaxLength(50)]
+    public string Name { get; set; }
+
+    [Required]
+    [MinLength(4)]
+    [MaxLength(50)]
+    public string Username { get; set; }
+
+    [Required]
+    [MinLength(4)]
+    [MaxLength(50)]
+    public string Password { get; set; }
+
+    [Required]
+    [MinLength(4)]
+    [MaxLength(50)]
+    public string Confirm { get; set; }
+  }
+}
