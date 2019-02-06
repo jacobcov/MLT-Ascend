e63effee1e756b26762bf190a50e05e52ac92697
Nate Johnson <johnson.nate.c@gmail.com>
2019-02-06 00:38:35 -0500

    created a server-side password validator [#163749094]

---
+++ MLTAscend/MLTAscend.MVC/Validators/PasswordChars.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace MLTAscend.MVC.Validators
+{
+  public class PasswordChars : ValidationAttribute
+  {
+    public override bool IsValid(object value)
+    {
+      var pass = value.ToString();
+
+      if (pass == null)
+      {
+        return false;
+      }
+      
+      return Regex.IsMatch(pass, "(^[0-9a-zA-z!@#$%^&*]+$)");
+    }
+  }
+}
