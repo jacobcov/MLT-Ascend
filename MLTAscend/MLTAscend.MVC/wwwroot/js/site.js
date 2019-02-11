fd358fc706062453186964063c5fedb561db647d
Nate Johnson <johnson.nate.c@gmail.com>
2019-02-10 23:44:55 -0500

    add jquery sort plug-in [#163854121]

--- MLTAscend/MLTAscend.MVC/wwwroot/js/site.js
+++ MLTAscend/MLTAscend.MVC/wwwroot/js/site.js
@@ -1,4 +1,29 @@
-﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
+﻿var table = $('table');
 
-// Write your JavaScript code.
+$('#companyName, #ticker, #date, #1d, #1w, #1mo, #3mo, #6mo, #1yr, #2yr, #5yr')
+  .each(function () {
+
+    var th = $(this),
+      thIndex = th.index(),
+      inverse = false;
+
+    th.click(function () {
+
+      table.find('td').filter(function () {
+
+        return $(this).index() === thIndex;
+
+      }).sortElements(function (a, b) {
+
+        return $.text([a]) > $.text([b]) ?
+          inverse ? -1 : 1
+          : inverse ? 1 : -1;
+
+      }, function () {
+
+        return this.parentNode.parentNode;
+      });
+
+      inverse = !inverse;
+    });
+  });
