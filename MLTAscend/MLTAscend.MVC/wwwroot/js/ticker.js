16491f1aad4228a5c961805027457cbe647818b6
Nate Johnson <johnson.nate.c@gmail.com>
2019-02-11 09:25:27 -0500

    add a fetch call to the alpha advantage api [#163861887]

---
+++ MLTAscend/MLTAscend.MVC/wwwroot/js/ticker.js
@@ -0,0 +1,10 @@
+﻿fetch('https://www.alphavantage.co/query?function=SMA&symbol=irbt&interval=daily&time_period=10&series_type=close&apikey=8YPLUQI35GBY540Q')
+  .then(response => {
+    if (response.ok) {
+      return response.json()
+    } else {
+      return Promise.reject('something went wrong!')
+    }
+  })
+  .then(data => console.log('data is', data))
+  .catch(error => console.log('error is', error));
