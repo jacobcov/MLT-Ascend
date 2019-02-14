bfcbdc7b0d8e10bdd078812e2514ea34570604db
Jacob <jacobdotcovington@gmail.com>
2019-02-14 14:24:14 -0500

    began working on charts [#163981398]

--- MLTAscend/MLTAscend.Trainer/Program.cs
+++ MLTAscend/MLTAscend.Trainer/Program.cs
@@ -14,31 +14,31 @@ namespace MLTAscend.Trainer
 
          StockData dataSample = new StockData()
          {
-            open = 103.86F,
-            high = 104.88F,
-            low = 103.2445F,
-            close = 104.27F,
-            timestamp = "2019-01-09",
-            volume = 32280840
+            open = 35.01F,
+            high = 35.65F,
+            low = 34.88F,
+            close = 35.48F,
+            timestamp = "2006-06-20",
+            volume = 4465500
          };
          var path = "../../../PredictionModels/OneDayPred_model.zip";
-         PredictionModelTrainer.TrainAndSaveModel(mlContext, "../../../Data/daily_MSFT.csv", path);
+         PredictionModelTrainer.TrainAndSaveModel(mlContext, "../../../Data/OneDayData.csv", path);
          PredictionModelTrainer.TestPrediction(mlContext, dataSample, path);
 
          path = "../../../PredictionModels/OneWeekPred_model.zip";
-         PredictionModelTrainer.TrainAndSaveModel(mlContext, "../../../Data/daily_MSFT.csv", path);
+         PredictionModelTrainer.TrainAndSaveModel(mlContext, "../../../Data/OneWeekData.csv", path);
          PredictionModelTrainer.TestPrediction(mlContext, dataSample, path);
 
          path = "../../../PredictionModels/OneMonthPred_model.zip";
-         PredictionModelTrainer.TrainAndSaveModel(mlContext, "../../../Data/daily_MSFT.csv", path);
+         PredictionModelTrainer.TrainAndSaveModel(mlContext, "../../../Data/OneMonthData.csv", path);
          PredictionModelTrainer.TestPrediction(mlContext, dataSample, path);
 
          path = "../../../PredictionModels/ThreeMonthPred_model.zip";
-         PredictionModelTrainer.TrainAndSaveModel(mlContext, "../../../Data/daily_MSFT.csv", path);
+         PredictionModelTrainer.TrainAndSaveModel(mlContext, "../../../Data/ThreeMonthData.csv", path);
          PredictionModelTrainer.TestPrediction(mlContext, dataSample, path);
 
          path = "../../../PredictionModels/OneYearPred_model.zip";
-         PredictionModelTrainer.TrainAndSaveModel(mlContext, "../../../Data/daily_MSFT.csv", path);
+         PredictionModelTrainer.TrainAndSaveModel(mlContext, "../../../Data/OneYearData.csv", path);
          PredictionModelTrainer.TestPrediction(mlContext, dataSample, path);
 
          Console.ReadLine();
