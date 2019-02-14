2a0500b79ee99e7205210b35936f685551cda492
Nate Johnson <johnson.nate.c@gmail.com>
2019-02-14 14:42:23 -0500

    updated with relative path [finish #163988731]

--- MLTAscend/MLTAscend.MVC/Startup.cs
+++ MLTAscend/MLTAscend.MVC/Startup.cs
@@ -44,7 +44,7 @@ namespace MLTAscend.MVC
       }
       else
       {
-        app.UseExceptionHandler("/Home/Error");
+        app.UseExceptionHandler("~/Home/Error");
         // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
         app.UseHsts();
       }
