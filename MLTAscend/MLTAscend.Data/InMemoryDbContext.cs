62dce8d32ef097cc780875047036a1e2ea35f34d
Mason Degeneffe <degeneffem@gmail.com>
2019-02-18 19:15:43 -0500

    remove commented code

--- MLTAscend/MLTAscend.Data/InMemoryDbContext.cs
+++ MLTAscend/MLTAscend.Data/InMemoryDbContext.cs
@@ -7,21 +7,20 @@ using Microsoft.EntityFrameworkCore.SqlServer;
 using Microsoft.EntityFrameworkCore.InMemory;
 namespace MLTAscend.Data
 {
-    public class InMemoryDbContext : DbContext
-    {
-        public DbSet<User> Users { get; set; }
-        public DbSet<Prediction> Predictions { get; set; }
+   public class InMemoryDbContext : DbContext
+   {
+      public DbSet<User> Users { get; set; }
+      public DbSet<Prediction> Predictions { get; set; }
 
-        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-        {
-            optionsBuilder.UseInMemoryDatabase("TestingDb");
-        }
+      protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+      {
+         optionsBuilder.UseInMemoryDatabase("TestingDb");
+      }
 
-        protected override void OnModelCreating(ModelBuilder modelBuilder)
-        {
-            modelBuilder.Entity<User>().HasKey(e => e.Id);
-            modelBuilder.Entity<Prediction>().HasKey(e => e.Id);
-        }
-    }
+      protected override void OnModelCreating(ModelBuilder modelBuilder)
+      {
+         modelBuilder.Entity<User>().HasKey(e => e.Id);
+         modelBuilder.Entity<Prediction>().HasKey(e => e.Id);
+      }
+   }
 }
-//optionsBuilder.UseInMemoryDatabase("server=dotnet2019mason.database.windows.net;database=MLTAscendDB;user id=sqladmin; password=Florida2019;");
