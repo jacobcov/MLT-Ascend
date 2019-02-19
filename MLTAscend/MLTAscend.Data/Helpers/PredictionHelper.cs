d701e2c671b7866bae12efb411f724a15112761d
Mason Degeneffe <degeneffem@gmail.com>
2019-02-18 19:07:28 -0500

    remove duplicate methods from merge

--- MLTAscend/MLTAscend.Data/Helpers/PredictionHelper.cs
+++ MLTAscend/MLTAscend.Data/Helpers/PredictionHelper.cs
@@ -8,87 +8,82 @@ using System.Linq;
 
 namespace MLTAscend.Data.Helpers
 {
-    public class PredictionHelper
-    {
-        public MLTAscendDbContext ExtContext { get; set; }
-        public InMemoryDbContext IntContext { get; set; }
+   public class PredictionHelper
+   {
+      public MLTAscendDbContext ExtContext { get; set; }
+      public InMemoryDbContext IntContext { get; set; }
 
-        public PredictionHelper()
-        {
-            ExtContext = new MLTAscendDbContext(MLTAscendDbContext.Configuration);
-            IntContext = null;
-        }
+      public PredictionHelper()
+      {
+         ExtContext = new MLTAscendDbContext(MLTAscendDbContext.Configuration);
+         IntContext = null;
+      }
 
-        public PredictionHelper(InMemoryDbContext context)
-        {
-            IntContext = context;
-            ExtContext = null;
-        }
+      public PredictionHelper(InMemoryDbContext context)
+      {
+         IntContext = context;
+         ExtContext = null;
+      }
 
-        public dom.Prediction GetPredictionByTicker(string ticker)
-        {
-            if (ExtContext != null && IntContext == null)
-            {
-                return ExtContext.Predictions.LastOrDefault(m => m.Ticker == ticker);
-            }
-            else
-            {
-                return IntContext.Predictions.LastOrDefault(m => m.Ticker == ticker);
-            }
-        }
+      public dom.Prediction GetPredictionByTicker(string ticker)
+      {
+         if (ExtContext != null && IntContext == null)
+         {
+            return ExtContext.Predictions.LastOrDefault(m => m.Ticker == ticker);
+         }
+         else
+         {
+            return IntContext.Predictions.LastOrDefault(m => m.Ticker == ticker);
+         }
+      }
 
-        public bool SetPrediction(dom.Prediction prediction, string username)
-        {
-            prediction.CreationDate = DateTime.Now;
+      public bool SetPrediction(dom.Prediction prediction, string username)
+      {
+         prediction.CreationDate = DateTime.Now;
 
-            if (ExtContext != null && IntContext == null)
-            {
-                UserHelper uh = new UserHelper();
+         if (ExtContext != null && IntContext == null)
+         {
+            UserHelper uh = new UserHelper();
 
-                var usr = uh.GetUserByUsername(username);
-                prediction.User = usr;
-                var e = ExtContext.Entry<dom.Prediction>(prediction).Entity;
+            var usr = uh.GetUserByUsername(username);
+            prediction.User = usr;
+            var e = ExtContext.Entry<dom.Prediction>(prediction).Entity;
 
-                e.User = usr;
-                ExtContext.Predictions.Attach(e).State = EntityState.Added;
+            e.User = usr;
+            ExtContext.Predictions.Attach(e).State = EntityState.Added;
 
-                return ExtContext.SaveChanges() > 0;
-            }
-            else
-            {
-                var uh = new UserHelper(new InMemoryDbContext());
+            return ExtContext.SaveChanges() > 0;
+         }
+         else
+         {
+            var uh = new UserHelper(new InMemoryDbContext());
 
-                var us = uh.GetUserByUsername(username);
-                prediction.User = us;
+            var us = uh.GetUserByUsername(username);
+            prediction.User = us;
 
-                var e = IntContext.Entry<dom.Prediction>(prediction).Entity;
+            var e = IntContext.Entry<dom.Prediction>(prediction).Entity;
 
-                e.User = us;
-                IntContext.Predictions.Attach(e).State = EntityState.Added;
-                return IntContext.SaveChanges() > 0;
-            }
-        }
+            e.User = us;
+            IntContext.Predictions.Attach(e).State = EntityState.Added;
+            return IntContext.SaveChanges() > 0;
+         }
+      }
 
-        public bool SetAnonymousPrediction(dom.Prediction prediction)
-        {
-            return SetPrediction(prediction, "anonymous");
-        }
+      public bool SetAnonymousPrediction(dom.Prediction prediction)
+      {
+         return SetPrediction(prediction, "anonymous");
+      }
 
-        public bool SetAnonymousPrediction(dom.Prediction prediction)
-        {
-            return SetPrediction(prediction, "anonymous");
-        }
-
-        public List<dom.Prediction> GetPredictions()
-        {
-            if (ExtContext != null && IntContext == null)
-            {
-                return ExtContext.Predictions.ToList();
-            }
-            else
-            {
-                return IntContext.Predictions.ToList();
-            }
-        }
-    }
+      public List<dom.Prediction> GetPredictions()
+      {
+         if (ExtContext != null && IntContext == null)
+         {
+            return ExtContext.Predictions.ToList();
+         }
+         else
+         {
+            return IntContext.Predictions.ToList();
+         }
+      }
+   }
 }
--- MLTAscend/MLTAscend.Data/Helpers/UserHelper.cs
+++ MLTAscend/MLTAscend.Data/Helpers/UserHelper.cs
@@ -6,90 +6,85 @@ using System.Linq;
 
 namespace MLTAscend.Data.Helpers
 {
-    public class UserHelper
-    {
-        public MLTAscendDbContext ExtContext { get; set; }
-        public InMemoryDbContext IntContext { get; set; }
+   public class UserHelper
+   {
+      public MLTAscendDbContext ExtContext { get; set; }
+      public InMemoryDbContext IntContext { get; set; }
 
-        public UserHelper()
-        {
-            ExtContext = new MLTAscendDbContext(MLTAscendDbContext.Configuration);
-            IntContext = null;
-        }
+      public UserHelper()
+      {
+         ExtContext = new MLTAscendDbContext(MLTAscendDbContext.Configuration);
+         IntContext = null;
+      }
 
-        public UserHelper(InMemoryDbContext context)
-        {
-            IntContext = context;
-            ExtContext = null;
-        }
+      public UserHelper(InMemoryDbContext context)
+      {
+         IntContext = context;
+         ExtContext = null;
+      }
 
-        public dom.User GetUserByUsername(string username)
-        {
+      public dom.User GetUserByUsername(string username)
+      {
+         if (ExtContext != null && IntContext == null)
+         {
+            return ExtContext.Users.FirstOrDefault(m => m.Username == username);
+         }
+         else
+         {
+            return IntContext.Users.FirstOrDefault(m => m.Username == username);
+         }
+      }
+
+      public bool SetUser(dom.User user)
+      {
+         var checkuser = GetUserByUsername(user.Username);
+         if (checkuser != null && checkuser.Username == user.Username)
+         {
+            return false;
+         }
+         else
+         {
+            user.CreationDate = DateTime.Now;
             if (ExtContext != null && IntContext == null)
             {
-                return ExtContext.Users.FirstOrDefault(m => m.Username == username);
+               ExtContext.Users.Add(user);
+               return ExtContext.SaveChanges() > 0;
             }
             else
             {
-                return IntContext.Users.FirstOrDefault(m => m.Username == username);
+               IntContext.Users.Add(user);
+               return IntContext.SaveChanges() > 0;
             }
-        }
+         }
+      }
 
-        public bool SetUser(dom.User user)
-        {
-            var checkuser = GetUserByUsername(user.Username);
-            if (checkuser != null && checkuser.Username == user.Username)
-            {
-                return false;
-            }
-            else
-            {
-                user.CreationDate = DateTime.Now;
-                if (ExtContext != null && IntContext == null)
-                {
-                    ExtContext.Users.Add(user);
-                    return ExtContext.SaveChanges() > 0;
-                }
-                else
-                {
-                    IntContext.Users.Add(user);
-                    return IntContext.SaveChanges() > 0;
-                }
-            }
-        }
+      public List<dom.Prediction> GetUserPredictions(string username)
+      {
+         if (ExtContext != null && IntContext == null)
+         {
+            return ExtContext.Predictions.Where(m => m.User.Username == username).ToList();
+         }
+         else
+         {
+            return IntContext.Predictions.Where(m => m.User.Username == username).ToList();
+         }
+      }
 
-        public List<dom.Prediction> GetUserPredictions(string username)
-        {
-            if (ExtContext != null && IntContext == null)
-            {
-                return ExtContext.Predictions.Where(m => m.User.Username == username).ToList();
-            }
-            else
-            {
-                return IntContext.Predictions.Where(m => m.User.Username == username).ToList();
-            }
-        }
+      public List<dom.Prediction> GetAnonymousPredictions()
+      {
+         return GetUserPredictions("anonymous");
+      }
 
-        public List<dom.Prediction> GetAnonymousPredictions()
-        {
-            return GetUserPredictions("anonymous");
-        }
-
-        public List<dom.Prediction> GetAnonymousPredictions()
-        {
-            return GetUserPredictions("anonymous");
-        }
-
-        public List<dom.User> GetUsers()
-        {
-            if (ExtContext != null && IntContext == null)
-            {
-                return ExtContext.Users.ToList();
-            }
-            else
-            {
-                return IntContext.Users.ToList();
-            }
-        }
-    }
+      public List<dom.User> GetUsers()
+      {
+         if (ExtContext != null && IntContext == null)
+         {
+            return ExtContext.Users.ToList();
+         }
+         else
+         {
+            return IntContext.Users.ToList();
+         }
+      }
+   }
 }
